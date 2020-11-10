20276325b8672004bffdeab8d4bbf84a69b3ec15
wangbaoquan <wbq1098@163.com>
2020-11-10 09:55:05 +0800

    init

---
+++ example/android/app/src/main/java/com/nxzybd/shipping_location_plugin_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nxzybd.shipping_location_plugin_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/ShippingLocationPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface ShippingLocationPlugin : NSObject<FlutterPlugin>
+@end
